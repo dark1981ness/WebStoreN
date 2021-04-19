fc58809cbcbfcfb2f8a24aef5798adf1a4f388c5
Konstantin <kostik06@gmail.com>
2021-04-19 15:03:24 +0300

    Регистрация клиента каталога товаров

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using WebStore.Clients.Employees;
+using WebStore.Clients.Products;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
@@ -70,7 +71,8 @@ namespace WebStore
             //services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
             //services.AddTransient<IProductData, InMemoryProductData>();
             services.AddTransient<IEmployeesData, EmployeesClient>();
-            services.AddScoped<IProductData, SqlProductData>();
+            //services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<IProductData, ProductsClient>();
             services.AddScoped<ICartServices, InCookiesCartService>();
             services.AddScoped<IOrderService, SqlOrderService>();
             services.AddScoped<IValuesService, ValuesClient>();
