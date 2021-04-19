d7302a566f450098abdd398c97647720365a998a
Konstantin <kostik06@gmail.com>
2021-04-19 14:46:39 +0300

    Контроллер каталога товаров WebAPI

--- Services/WebStore.Interfaces/WebAPI.cs
+++ Services/WebStore.Interfaces/WebAPI.cs
@@ -4,5 +4,7 @@
     {
         public const string Employees = "api/employees";
         public const string TestWebAPI = "api/values";
+        public const string Products = "api/products";
+
     }
 }
---
+++ Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain;
+using WebStore.Domain.DTO;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Products)]
+    [ApiController]
+    public class ProductsApiController : ControllerBase, IProductData
+    {
+        private readonly IProductData _productData;
+
+        public ProductsApiController(IProductData productData) => _productData = productData;
+
+        [HttpGet("brands")]
+        public IEnumerable<BrandDTO> GetBrands() => _productData.GetBrands();
+
+        [HttpGet("{id}")]
+        public ProductDTO GetProductById(int id) => _productData.GetProductById(id);
+
+        [HttpPost]
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter productFilter = null) => _productData.GetProducts(productFilter);
+
+        [HttpGet("sections")]
+        public IEnumerable<SectionDTO> GetSections() => _productData.GetSections();
+    }
+}
