970e0d916062e9d447ff78b078bdc842a8de0afc
Konstantin <kostik06@gmail.com>
2021-04-19 13:02:38 +0300

    DTO для работы WebAPI каталога товаров

---
+++ Common/WebStore.Domain/DTO/BrandDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain.DTO
+{
+    public class BrandDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+    }
+
+    public class SectionDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public BrandDTO Brand { get; set; }
+
+        public SectionDTO Section { get; set; }
+    }
+}
