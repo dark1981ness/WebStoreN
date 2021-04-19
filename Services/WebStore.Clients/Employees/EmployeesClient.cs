7c56b4083fe3c819de5a094476e18838e821d203
Konstantin <kostik06@gmail.com>
2021-04-19 12:01:05 +0300

    Клиент для работы с WebAPI управления сотрудниками

--- Services/WebStore.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -1,19 +1,37 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using WebStore.Clients.Base;
+using WebStore.Domain.Models;
 using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.Clients.Employees
 {
-    public class EmployeesClient: BaseClient
+    public class EmployeesClient: BaseClient, IEmployeesData
     {
         public EmployeesClient(IConfiguration configuration) : base(configuration, WebAPI.Employees)
         {
 
         }
+
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
+
+
+        public Employee Add(string lastName, string firstName, string patronymic, DateTime dOb, DateTime hireDate, decimal salary, string eMail) =>
+            Post($"{Address}/employee?LastName={lastName}&FirstName={firstName}&Patronymic={patronymic}", "")
+            .Content.ReadAsAsync<Employee>().Result;
+
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
+
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
+
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
+
+        public Employee GetByName(string lastName, string firstName, string patronymic) =>
+            Get<Employee>($"{Address}/employee?LastName={lastName}&FirstName={firstName}&Patronymic={patronymic}");
+
+        public void Update(Employee employee) => Put(Address, employee);
     }
 }
