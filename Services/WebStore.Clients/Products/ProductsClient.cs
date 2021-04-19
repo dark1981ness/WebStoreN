babf02d8916bf6ba67ae2d06f5a20697cc61a494
Konstantin <kostik06@gmail.com>
2021-04-19 15:00:20 +0300

    клиент каталога товаров

---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.DTO;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Products
+{
+    public class ProductsClient:BaseClient,IProductData
+    {
+
+        public ProductsClient(IConfiguration configuration) :base(configuration,WebAPI.Products) { }
+
+        public IEnumerable<BrandDTO> GetBrands()=> Get<IEnumerable<BrandDTO>>($"{Address}/brands");
+
+        public ProductDTO GetProductById(int id) => Get<ProductDTO>($"{Address}/{id}");
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter productFilter = null) =>
+            Post(Address, productFilter ?? new ProductFilter())
+            .Content
+            .ReadAsAsync<IEnumerable<ProductDTO>>()
+            .Result;
+
+        public IEnumerable<SectionDTO> GetSections() => Get<IEnumerable<SectionDTO>>($"{Address}/sections");
+        
+    }
+}
