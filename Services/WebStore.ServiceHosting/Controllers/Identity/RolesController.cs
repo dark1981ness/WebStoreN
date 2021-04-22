925a10bd1c6c20db2725436ac086bc2d218474a9
Konstantin <kostik06@gmail.com>
2021-04-22 12:32:31 +0300

    Заготовки контроллеров

---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/RolesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.Role)]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<Role> _roleStore;
+
+        public RolesController(WebStoreDB db)
+        {
+            _roleStore = new RoleStore<Role>(db);
+        }
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.User)]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDB> _userStore;
+        public UsersController(WebStoreDB db)
+        {
+            _userStore = new UserStore<User, Role, WebStoreDB>(db);
+            //_userStore.AutoSaveChanges = false;
+        }
+    }
+}
