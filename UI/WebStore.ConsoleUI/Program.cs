eeb9e4b9670358bed7fce0a024bbac9cc0cb2f78
Konstantin <kostik06@gmail.com>
2021-04-23 16:49:59 +0300

    Тестовый консольный клиент

---
+++ UI/WebStore.ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using WebStore.Clients.Products;
+
+namespace WebStore.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var products_client = new ProductsClient(configuration);
+
+            foreach (var product in products_client.GetProducts())
+            {
+                Console.WriteLine($"{product.Name} - {product.Price}");
+            }
+        }
+    }
+}
