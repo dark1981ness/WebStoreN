11c3325053f289f7f2197958e099bcd16cade0e0
Konstantin <kostik06@gmail.com>
2021-04-23 17:17:52 +0300

    Документация к WebAPI

--- Common/WebStore.Domain/DTO/OrderDTO.cs
+++ Common/WebStore.Domain/DTO/OrderDTO.cs
@@ -4,26 +4,76 @@ using WebStore.Domain.ViewModels;
 
 namespace WebStore.Domain.DTO
 {
+    /// <summary>
+    /// Информация о заказе
+    /// </summary>
     public class OrderDTO
     {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Имя заказа
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Телефон, указанный в заказе для связи
+        /// </summary>
         public string Phone { get; set; }
+
+        /// <summary>
+        /// Адрес доставки
+        /// </summary>
         public string Address { get; set; }
+
+        /// <summary>
+        /// Дата заказа
+        /// </summary>
         public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Перечень пунктов заказа
+        /// </summary>
         public IEnumerable<OrderItemDTO> Items { get; set; }
     }
 
+    /// <summary>
+    /// Пункт заказа
+    /// </summary>
     public class OrderItemDTO
     {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// Количество
+        /// </summary>
         public int Quantity { get; set; }
     }
 
+    /// <summary>
+    /// Модель процесса создания заказа
+    /// </summary>
     public class CreateOrderModel
     {
+        /// <summary>
+        /// Модель заказа
+        /// </summary>
         public OrderViewModel Order { get; set; }
+
+        /// <summary>
+        /// Перечень элементов заказа
+        /// </summary>
         public IList<OrderItemDTO> Items { get; set; }
     }
 }
--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
+using System.IO;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces.Services;
@@ -60,6 +61,28 @@ namespace WebStore.ServiceHosting
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore.ServiceHosting", Version = "v1" });
+                const string webstore_api_xml = "WebStore.ServiceHosting.xml";
+                const string webstore_domain_xml = "WebStore.Domain.xml";
+                const string debug_path = "bin/debug/net5.0";
+                //c.IncludeXmlComments("WebStore.ServiceHosting.xml");
+
+                if (File.Exists(webstore_api_xml))
+                {
+                    c.IncludeXmlComments(webstore_api_xml);
+                }
+                else if(File.Exists(Path.Combine(debug_path,webstore_api_xml)))
+                {
+                    c.IncludeXmlComments(Path.Combine(debug_path, webstore_api_xml));
+                }
+
+                if (File.Exists(webstore_domain_xml))
+                {
+                    c.IncludeXmlComments(webstore_domain_xml);
+                }
+                else if (File.Exists(Path.Combine(debug_path, webstore_domain_xml)))
+                {
+                    c.IncludeXmlComments(Path.Combine(debug_path, webstore_domain_xml));
+                }
             });
         }
 
