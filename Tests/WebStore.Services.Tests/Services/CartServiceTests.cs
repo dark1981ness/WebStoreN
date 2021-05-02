bf3a9e3b001c79f89073313b5cceaffe298e25f5
Konstantin <kostik06@gmail.com>
2021-05-03 01:44:38 +0300

    Модульные тесты сервиса корзины

--- Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using WebStore.Domain.DTO;
 using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
 using Assert = Xunit.Assert;
 
 namespace WebStore.Services.Tests.Services
@@ -20,6 +22,9 @@ namespace WebStore.Services.Tests.Services
         private Cart _cart;
 
         private Mock<IProductData> _productDataMock;
+        private Mock<ICartStore> _cartStoreMock;
+
+        private ICartServices _cartService;
 
         [TestInitialize]
         public void Initialize()
@@ -70,6 +75,10 @@ namespace WebStore.Services.Tests.Services
                     },
 
                 });
+            _cartStoreMock = new Mock<ICartStore>();
+            _cartStoreMock.Setup(c => c.Cart).Returns(_cart);
+
+            _cartService = new CartService(_cartStoreMock.Object, _productDataMock.Object);
         }
 
         [TestMethod]
@@ -117,6 +126,87 @@ namespace WebStore.Services.Tests.Services
             var actual_total_price = cart_view_model.TotalPrice;
 
             Assert.Equal(expected_total_price, actual_total_price);
+        }
+
+        [TestMethod]
+        public void CartService_Add_WorkCorrect()
+        {
+            _cart.Items.Clear();
+
+            const int expected_id = 5;
+            const int expected_items_count = 1;
+
+            _cartService.Add(expected_id);
+
+            Assert.Equal(expected_items_count, _cart.ItemsCount);
+
+            Assert.Single(_cart.Items);
+
+            Assert.Equal(expected_id, _cart.Items.First().ProductId);
+        }
+
+        [TestMethod]
+        public void CartService_Remove_Correct_Item()
+        {
+            const int item_id = 1;
+            const int expected_product_id = 2;
+
+            _cartService.Remove(item_id);
+
+            Assert.Single(_cart.Items);
+
+            Assert.Equal(expected_product_id, _cart.Items.Single().ProductId);
+        }
+
+        [TestMethod]
+        public void CartService_Clear_ClearCart()
+        {
+            _cartService.Clear();
+
+            Assert.Empty(_cart.Items);
+        }
+
+        [TestMethod]
+        public void CartService_Decrement_Correct()
+        {
+            const int item_id = 2;
+            const int expected_quantity = 2;
+            const int expectes_items_count = 3;
+            const int expected_products_count = 2;
+
+            _cartService.Decrement(item_id);
+
+            Assert.Equal(expectes_items_count, _cart.ItemsCount);
+            Assert.Equal(expected_products_count, _cart.Items.Count);
+            var items = _cart.Items.ToArray();
+            Assert.Equal(item_id, items[1].ProductId);
+            Assert.Equal(expected_quantity, items[1].Quantity);
+        }
+
+        [TestMethod]
+        public void CartService_Remove_Item_When_Decrement_to_0()
+        {
+            const int item_id = 1;
+            const int expected_items_count = 3;
+
+            _cartService.Decrement(item_id);
+
+            Assert.Equal(expected_items_count, _cart.ItemsCount);
+            Assert.Single(_cart.Items);
+        }
+
+        [TestMethod]
+        public void CartService_GetViewModel_WorkCorrect()
+        {
+            Debug.WriteLine("Тестирование преобразования корзины в модель-представления");
+
+            const int expected_items_count = 4;
+            const decimal expected_first_product_price = 1.1m;
+
+            var result = _cartService.GetViewModel();
+
+            Assert.Equal(expected_items_count, result.ItemsCount);
+            Assert.Equal(expected_first_product_price, result.Items.First().product.Price);
         }
     }
 }
