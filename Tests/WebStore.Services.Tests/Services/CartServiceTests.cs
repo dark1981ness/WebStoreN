65c67e8cb1ee135aed1a3ee0ac58304a94d4fec2
Konstantin <kostik06@gmail.com>
2021-05-03 00:54:03 +0300

    Подготовка к тестированию сервиса корзины

---
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain;
+using WebStore.Domain.DTO;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests.Services
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        private Cart _cart;
+
+        private Mock<IProductData> _productDataMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new() { ProductId = 1, Quantity = 1 },
+                    new() { ProductId = 2, Quantity = 3 },
+                }
+            };
+
+            _productDataMock = new Mock<IProductData>();
+            _productDataMock
+                .Setup(c => c.GetProducts(It.IsAny<ProductFilter>()))
+                .Returns(new[]
+                {
+                    new ProductDTO
+                    {
+                        Id = 1,
+                        Name = "Product 1",
+                        Price = 1.1m,
+                        Order = 1,
+                        ImageUrl = "Img_1.png",
+                        Brand = new BrandDTO { Id = 1, Name = "Brand 1", Order = 1 },
+                        Section = new SectionDTO{Id = 1, Name = "Section 1", Order = 1 }
+                    },
+                    new ProductDTO
+                    {
+                        Id = 2,
+                        Name = "Product 2",
+                        Price = 2.2m,
+                        Order = 2,
+                        ImageUrl = "Img_1.png",
+                        Brand = new BrandDTO { Id = 2, Name = "Brand 2", Order = 2 },
+                        Section = new SectionDTO{Id = 2, Name = "Section 2", Order = 2 }
+                    },
+                    new ProductDTO
+                    {
+                        Id =31,
+                        Name = "Product 3",
+                        Price = 3.3m,
+                        Order = 3,
+                        ImageUrl = "Img_3.png",
+                        Brand = new BrandDTO { Id = 3, Name = "Brand 3", Order = 3 },
+                        Section = new SectionDTO{Id = 3, Name = "Section 3", Order = 3 }
+                    },
+
+                });
+        }
+
+        [TestMethod]
+        public void Cart_Class_ItemsCount_returns_Correct_Quantity()
+        {
+            var cart = _cart;
+            var expected_items_count = _cart.Items.Sum(i => i.Quantity);
+
+            var actual_items_count = cart.ItemsCount;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_ItemsCount()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+            const int expected_count = 4;
+
+            var actual_count = cart_view_model.ItemsCount;
+
+            Assert.Equal(expected_count, actual_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_TotalPrice()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+            var expected_total_price = cart_view_model.Items.Sum(item => item.quantity * item.product.Price);
+
+            var actual_total_price = cart_view_model.TotalPrice;
+
+            Assert.Equal(expected_total_price, actual_total_price);
+        }
+    }
+}
