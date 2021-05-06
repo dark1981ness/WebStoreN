8c76a87fe2b2e57c09788a045defbe84682d88a9
Konstantin <kostik06@gmail.com>
2021-05-06 14:56:09 +0300

    Карта сайта

---
+++ UI/WebStore/Controllers/API/SiteMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Controllers.API
+{
+    public class SiteMap : ControllerBase
+    {
+        public IActionResult Index([FromServices] IProductData ProductData)
+        {
+            var nodes = new List<SitemapNode>
+            {
+                new (Url.Action("Index", "Home")),
+                new (Url.Action("SecondAction", "Home")),
+                new (Url.Action("Index", "Catalog")),
+                new (Url.Action("Index", "WebAPI")),
+            };
+
+            nodes.AddRange(ProductData.GetSections().Select(s => new SitemapNode(Url.Action("Index", "Catalog", new { SectionId = s.Id }))));
+
+            foreach (var brand in ProductData.GetBrands())
+                nodes.Add(new SitemapNode(Url.Action("Index", "Catalog", new { BrandId = brand.Id })));
+
+            foreach (var product in ProductData.GetProducts())
+                nodes.Add(new SitemapNode(Url.Action("Index", "Catalog", new { product.Id })));
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
