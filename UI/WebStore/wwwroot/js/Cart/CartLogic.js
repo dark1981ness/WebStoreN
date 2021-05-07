f251a6d32d67a35bfbf9747543b9624c0b1f682b
Konstantin <kostik06@gmail.com>
2021-05-07 14:15:35 +0300

    Асинхронное доабвление товаров в корзину из каталога

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -0,0 +1,44 @@
+﻿Cart = {
+    _properties: {
+        getCartViewLink: "",
+        addToCartLink: "",
+        decrementLink: "",
+        removeFromCartLink: ""
+    },
+
+    init: function (properties) {
+        $.extend(Cart._properties, properties);
+
+        $(".add-to-cart").click(Cart.addToCart);
+    },
+
+    addToCart: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                Cart.showToolTip(button);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("addToCart fail"); });
+    },
+
+    showToolTip: function (button) {
+        button.tooltip({title:"Add to cart!"}).tooltip("show");
+        setTimeout(function () {
+            button.tooltip("destroy");
+        }, 500);
+    },
+
+    refreshCartView: function () {
+        var container = $("#cart-container");
+        $.get(Cart._properties.getCartViewLink)
+            .done(function (cartHtml) {
+                container.html(cartHtml);
+            })
+            .fail(function () { console.log("refreshCartView fail"); });
+    }
+}
