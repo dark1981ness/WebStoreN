2fd9c51ea9aa10bb921dbe93e864ced84c96f2ba
Konstantin <kostik06@gmail.com>
2021-05-07 14:33:46 +0300

    Асинхронное увеличение количества товара в корзине

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -8,8 +8,14 @@
 
     init: function (properties) {
         $.extend(Cart._properties, properties);
+        Cart.initEvents();
+    },
 
+    initEvents: function () {
         $(".add-to-cart").click(Cart.addToCart);
+        $(".cart_quantity_up").click(Cart.incrementItem);
+        $(".cart_quantity_down").click();
+        $(".cart_quantity_delete").click();
     },
 
     addToCart: function (event) {
@@ -27,7 +33,7 @@
     },
 
     showToolTip: function (button) {
-        button.tooltip({title:"Add to cart!"}).tooltip("show");
+        button.tooltip({ title: "Add to cart!" }).tooltip("show");
         setTimeout(function () {
             button.tooltip("destroy");
         }, 500);
@@ -40,5 +46,49 @@
                 container.html(cartHtml);
             })
             .fail(function () { console.log("refreshCartView fail"); });
+    },
+
+    incrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        var tr = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                const count = parseInt($(".cart_quantity_input", tr).val());
+                $(".cart_quantity_input", tr).val(count + 1);
+
+                Cart.refreshPrice(tr);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("incrementItem fail"); });
+    },
+
+    refreshPrice: function (container) {
+        const count = parseInt($(".cart_quantity_input", container).val());
+        const price = parseFloat($(".cart_price", container).data("price"));
+        const totalPrice = count * price;
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        const cartTotalPrice = $(".cart_total_price", container);
+        cartTotalPrice.data("price", totalPrice);
+        cartTotalPrice.html(value);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var totalPrice = 0;
+
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        $("#total-order-price").html(value);
     }
 }
